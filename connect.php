2771591e2305bb8ada09e58c510b102f15ee0c50
Адилет Талипович <110617709+Redneak44@users.noreply.github.com>
2022-08-31 23:20:05 +0600

    Delete connect.php

--- connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$connect = mysqli_connect(hostname:'localhost', username:'root', password:'root', database:'project' );
-if (!$connect){
-    die('Error connect to database');
-}
-?>
