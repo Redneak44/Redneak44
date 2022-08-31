3dca9896f259d51a069b3758baa5e7a148bb7f02
Адилет Талипович <110617709+Redneak44@users.noreply.github.com>
2022-08-31 23:23:37 +0600

    Delete delete_request.php

--- delete_request.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '../../../connect.php';
-
-$id = $_GET['id'];
-
-mysqli_query($connect, query: "DELETE FROM request WHERE `request`.`id` = '$id'");
-header('location:view_request.php');
-?>
